eaf686ee8037ca7bd914a3096703f1e92edd579c
saipulfahmi <80950323+saipulfahmi@users.noreply.github.com>
2021-03-19 11:19:36 +0200

    Add files via upload

---
+++ tugas_01bag.py
@@ -0,0 +1,10 @@
+hari = ["Senin","Selasa", "Rabu", "Kamis", "Jum'at", "Sabtu", "Minggu"]
+for i in range(len(hari)):
+    print(i + 1,hari[i])
+    if i== 4: 
+        print("     hari masuk kuliah")
+    elif i == 6:
+        print("     Hari libur kuliah")
+        
+        
+        
