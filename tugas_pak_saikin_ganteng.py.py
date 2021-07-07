080e0d68f096a069924b41a3874ac933e39c2957
saipulfahmi <80950323+saipulfahmi@users.noreply.github.com>
2021-07-07 22:33:48 +0700

    Add files via upload

---
+++ tugas_pak_saikin_ganteng.py.py
@@ -0,0 +1,34 @@
+Python 3.9.2 (tags/v3.9.2:1a79785, Feb 19 2021, 13:44:55) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=========================== RESTART: F:\TUGAS\tree.py ==========================
+masuk cuaca :cerah
+hujan :hgtf
+lembab :h
+angin :h
+cerah :hu
+['cerah', ['h', ['hgtf', [], []], []], ['hu', ['h', [], []], []]]
+hasil : bisa main
+mau ulang kah??y
+masuk cuaca :cerah
+hujan :g
+lembab :fg
+angin :
+=========================== RESTART: F:\TUGAS\tree.py ==========================
+masuk cuaca :hujan
+main bola :g
+lembab :y
+angin :u
+cerah :i
+['hujan', ['y', ['g', [], []], []], ['i', ['u', [], []], []]]
+hasil : tidak main
+mau ulang kah??
+=========================== RESTART: F:\TUGAS\tree.py ==========================
+masuk cuaca :cerah
+juru kiri :k
+juru kanan :g
+juru atas :h
+juru bawah :v
+['cerah', ['g', ['k', [], []], []], ['v', ['h', [], []], []]]
+hasil : bisa main
+mau ulang kah??
